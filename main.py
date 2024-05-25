9abf8668e50bc4ccd31ad91376df4c5f22fc0eb9
Sarah B <barker.sarah.ms@gmail.com>
2024-05-25 16:22:27 -0400

    connecting?

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ print("Welcome, " + name + "." " Which fruit would you like to buy?")
 fruit_choice=['1. Apple $2', '2. Grape $1', '3. Orange $3']
 for fruit in fruit_choice:
     print(fruit)
-
+#Ask Again
 while True:
     selection = input("Enter the number of your choice or 'N' to checkout: ")
 
@@ -34,10 +34,9 @@ while True:
         print("Invalid input. Please enter a number from 1 to 3 or 'N' to checkout.")
 
 
-#Ask Again
-
-
-
-
-
-#Once they are done, print how many of each fruit, along with the subtotal and the total cost
+#Once they are done, Print "Order for {name}, seperate line,
+#1 apple(s) at $2 apiece
+#etc
+#subtotal: $5
+#5% tax: $.25
+#Total: $5.25
