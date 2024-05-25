c63532feb8514b5d544da3c056da2fb80a159f31
Sarah B <barker.sarah.ms@gmail.com>
2024-05-25 17:39:40 -0400

    connecting?

--- main.py
+++ main.py
@@ -29,8 +29,25 @@ while True:
         total = subtotal +tax
 
         print(f"\nOrder for {name}:")
+        apples_count = 0
+        grapes_count = 0
+        oranges_count = 0
+
         for item in order_summary:
-            print (item)
+            if 'Apple' in item:
+                apples_count += 1
+            elif 'Grape' in item:
+                grapes_count += 1
+            elif 'Orange' in item:
+                oranges_count += 1
+
+        if apples_count > 0:
+            print(f"{apples_count} Apple(s) at ${fruit_prices[1]['price']} apiece")
+        if grapes_count > 0:
+            print(f"{grapes_count} Grape(s) at ${fruit_prices[2]['price']} apiece")
+        if oranges_count > 0:
+            print(f"{oranges_count} Orange(s) at ${fruit_prices[3]['price']} apiece")
+
         print(f"Subtotal: ${subtotal:.2f}")
         print(f"5% Tax: ${tax:.2f}")
         print(f"Total: ${total:.2f}")
@@ -43,15 +60,21 @@ while True:
             running_total += item['price']
             order_summary.append(f"1 {item['name']} at ${item['price']} each")
             print(f"You bought 1 {item['name']} at ${item['price']}.")
+
+            choice = input("Would you like to buy another piece of fruit? Y or N: ").strip().upper()
+            if choice == 'Y':
+                print(f"Welcome {name}. Which fruit would you like to buy?")
+                for key, value in fruit_prices.items():
+                    print(f"{key}. {value['name']} ${value['price']}")
+            elif choice == 'N':
+                    continue
+            else:
+                print("Invalid input. Please enter 'Y' or 'N'.")
         else:
             print("Invalid selection. Please choose a number from 1 to 3 or 'N' to checkout.")
+
     except ValueError:
         print("Invalid input. Please enter a number from 1 to 3 or 'N' to checkout.")
-
-
-
-
-
 
 #Once they are done, Print "Order for {name}, separate line,
 #1 apple(s) at $2 apiece
